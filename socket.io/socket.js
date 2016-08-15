798d49703d47fb9e3f792209286092cb6082b3e9
zhilidali <1373653244@qq.com>
2016-08-15 11:25:32 +0800

    socket.io与终端的连接、断开的通信

--- socket.io/socket.js
+++ socket.io/socket.js
@@ -1,6 +1,21 @@
 var http = require('http');
+var fs = require('fs');
+
 var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('socket.io 示例', 'utf-8');
+	fs.readFile('./index.html',function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data, 'utf-8');
+	});
 }).listen(3000, '127.0.0.1');
 console.log('服务运行在http://127.0.0.1:3000');
+
+//将Socket.IO绑定到服务器
+var io = require('socket.io').listen(server);
+
+//让Socket.IO对特定事件和客户端消息做出响应
+io.sockets.on('connection', function(socket) {
+	console.log('客户端连接');
+	socket.on('disconnect', function() {
+		console.log('客户端断开');
+	});
+});
